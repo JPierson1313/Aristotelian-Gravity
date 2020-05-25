aa08a8113bff5bb58ef69cdc5279af7cf8c7ab18
JPierson1313 <jpierso3@masonlive.gmu.edu>
2020-05-24 22:53:22 -0400

    Change scripts

---
+++ AristotelianGravity/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Script used to reset the level whenever somebody clicks on the restart button in the game
+/// </summary>
+public class RestartLevel : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
